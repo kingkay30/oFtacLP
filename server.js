8753ec63138e50d7c994efc2b7545d78b0173b69
clinton <clintonararomi@yahoo.com>
2023-08-12 10:06:25 +0100

    staic

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 // Your static pre-build assets folder
-app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static(path.join(__dirname, "..", "build/static")));
 
 // Root Redirects to the pre-build assets
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "..", "build"));
+  res.sendFile(path.join(__dirname, "..", "build/static"));
 });
 
 // Any Page Redirects to the pre-build assets folder index.html that // will load the react app
