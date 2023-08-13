b8a35221aa3645c0a158e2a46649200b46b28855
clinton <clintonararomi@yahoo.com>
2023-08-13 11:07:03 +0100

    padding

--- src/components/CTA.js
+++ src/components/CTA.js
@@ -13,7 +13,7 @@ const CssTextField = styled(TextField)({
   "& .css-66dh3a-MuiInputBase-input-MuiInput-input": {
     color: "white",
     fontSize: 35,
-    padding: 10,
+    padding: 13,
   },
   "& .css-1d1r5q-MuiFormHelperText-root  ": {
     fontSize: 35,
